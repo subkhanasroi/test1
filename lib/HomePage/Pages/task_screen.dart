afa2f88bc950811551a8e5c2f5f0c80555f59877
subkhanasroi <asroisubkhan01@hgmail.com>
2021-03-26 14:59:22 +0700

    commit 5

--- lib/HomePage/Pages/task_screen.dart
+++ lib/HomePage/Pages/task_screen.dart
@@ -5,9 +5,42 @@ class TaskScreen extends StatefulWidget {
   _TaskScreenState createState() => _TaskScreenState();
 }
 
-class _TaskScreenState extends State<TaskScreen> {
+class _TaskScreenState extends State<TaskScreen> with TickerProviderStateMixin {
+  TabController _taskController;
+  var pages = [];
+
   @override
+  void initState() {
+    super.initState();
+    _taskController = new TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _taskController.dispose();
+  }
+
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        TabBar(
+          controller: _taskController,
+          indicatorColor: Colors.teal,
+          labelColor: Colors.teal,
+          unselectedLabelColor: Colors.black,
+          isScrollable: true,
+          tabs: <Widget>[
+            Tab(
+              text: "One",
+            ),
+            Tab(
+              text: "Two",
+            ),
+          ],
+        )
+      ],
+    );
   }
 }
